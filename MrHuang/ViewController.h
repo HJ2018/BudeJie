7d63c237ab6f3d40dc5b5fdd68fc0fe84b06a5a9
黄杰 <huangjie@mrhuangs-macbook-pro.local>
2016-05-26 11:32:09 +0800

    Initial Commit

---
+++ MrHuang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MrHuang
+//
+//  Created by MrHuang on 16/5/26.
+//  Copyright © 2016年 MrHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MrHuang/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MrHuang
+//
+//  Created by MrHuang on 16/5/26.
+//  Copyright © 2016年 MrHuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
