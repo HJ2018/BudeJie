36627ab225fe2afbcaeee0279e04cd4975d1ae1a
jie.huang <jie.huang@jiehuangdeMacBook-Pro.local>
2018-04-28 09:10:34 +0800

    增加清楚缓存功能

--- MrHuang/Classes/Me-我/Controller/HJMeconteoller.m
+++ MrHuang/Classes/Me-我/Controller/HJMeconteoller.m
@@ -17,6 +17,8 @@
 #import "UIImageView+WebCache.h"
 #import "XMGConst.h"
 #import <SafariServices/SafariServices.h>
+#import "SDImageCache.h"
+#define CachePath [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject]
 
 
 
@@ -45,7 +47,7 @@ static NSString * const ID = @"cell";
 {
     [super viewDidLoad];
     
-//    self.tableView.tableFooterView = [[HJMeFooterView alloc]init];
+    self.tableView.tableFooterView = [[HJMeFooterView alloc]init];
     
     
     self.tableView.sectionHeaderHeight = 0;
@@ -65,7 +67,7 @@ static NSString * const ID = @"cell";
     // 设置背景色
     self.view.backgroundColor = HJGBColor(223, 223, 223);
     
-    [self getData];
+//    [self getData];
 }
 
 
@@ -180,12 +182,33 @@ static NSString * const ID = @"cell";
         cell.textLabel.text = @"登录/注册";
         cell.imageView.image = [UIImage imageNamed:@"publish-audio"];
     } else {
-        cell.textLabel.text = @"离线下载";
+        cell.textLabel.text = [self sizeStr];
         // 只要有其他cell设置过imageView.image, 其他不显示图片的cell都需要设置imageView.image = nil
         cell.imageView.image = nil;
     }
     
     return cell;
+}
+
+//点击cell清楚缓存
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 清空缓存
+    // 获取文件管理者
+    NSFileManager *mgr = [NSFileManager defaultManager];
+    
+    // 获取cache文件夹下所有文件,不包括子路径的子路径
+    NSArray *subPaths = [mgr contentsOfDirectoryAtPath:CachePath error:nil];
+    
+    for (NSString *subPath in subPaths) {
+        // 拼接完成全路径
+        NSString *filePath = [CachePath stringByAppendingPathComponent:subPath];
+        
+        // 删除路径
+        [mgr removeItemAtPath:filePath error:nil];
+    }
+    
+    [self.tableView reloadData];
 }
 
 - (void)settingClick
@@ -201,5 +224,75 @@ static NSString * const ID = @"cell";
 - (void)moonClick
 {
     HJLog(@"444");
+}
+
+
+
+// 获取缓存尺寸字符串
+- (NSString *)sizeStr
+{
+    // 获取Caches文件夹路径
+    NSString *cachePath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    
+    NSInteger totalSize = [self getFileSize:cachePath];
+    
+    NSString *sizeStr = @"清除缓存";
+    // MB KB B
+    if (totalSize > 1000 * 1000) {
+        // MB
+        CGFloat sizeF = totalSize / 1000.0 / 1000.0;
+        sizeStr = [NSString stringWithFormat:@"%@(%.1fMB)",sizeStr,sizeF];
+    } else if (totalSize > 1000) {
+        // KB
+        CGFloat sizeF = totalSize / 1000.0;
+        sizeStr = [NSString stringWithFormat:@"%@(%.1fKB)",sizeStr,sizeF];
+    } else if (totalSize > 0) {
+        // B
+        sizeStr = [NSString stringWithFormat:@"%@(%.ldB)",sizeStr,totalSize];
+    }
+    
+    return sizeStr;
+}
+
+// 自己去计算SDWebImage做的缓存
+- (NSInteger)getFileSize:(NSString *)directoryPath
+{
+    // NSFileManager
+    // attributesOfItemAtPath:指定文件路径,就能获取文件属性
+    // 把所有文件尺寸加起来
+    
+    // 获取文件管理者
+    NSFileManager *mgr = [NSFileManager defaultManager];
+    
+    // 获取文件夹下所有的子路径,包含子路径的子路径
+    NSArray *subPaths = [mgr subpathsAtPath:directoryPath];
+    
+    NSInteger totalSize = 0;
+    
+    for (NSString *subPath in subPaths) {
+        // 获取文件全路径
+        NSString *filePath = [directoryPath stringByAppendingPathComponent:subPath];
+        
+        // 判断隐藏文件
+        if ([filePath containsString:@".DS"]) continue;
+        
+        // 判断是否文件夹
+        BOOL isDirectory;
+        // 判断文件是否存在,并且判断是否是文件夹
+        BOOL isExist = [mgr fileExistsAtPath:filePath isDirectory:&isDirectory];
+        if (!isExist || isDirectory) continue;
+        
+        // 获取文件属性
+        // attributesOfItemAtPath:只能获取文件尺寸,获取文件夹不对,
+        NSDictionary *attr = [mgr attributesOfItemAtPath:filePath error:nil];
+        
+        // 获取文件尺寸
+        NSInteger fileSize = [attr fileSize];
+        
+        totalSize += fileSize;
+    }
+    
+    return totalSize;
+    
 }
 @end
